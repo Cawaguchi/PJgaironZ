63d4c0073803afa867af3a9fe05eef97c575922e
ivy-kat <kataoka@ivy.ac.jp>
2025-11-15 22:00:51 +0900

    GameManagerのstatic変数修正に伴う変更

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 
 public class Main{
     public static void main(String args[]) {
-        GameManager.debugMode= true;
+        GameManager.sDebugMode=true;
         GameManager.showTitle();
 
         Player.chooseHands();
