1427c6552091c7c76ea18b49aeeae745f27f52c9
ivy-kat <kataoka@ivy.ac.jp>
2025-11-15 22:09:25 +0900

    Playerのメンバ変数修正に伴う変更

--- Display.java
+++ Display.java
@@ -1,8 +1,8 @@
 public class Display {
     public static void showResult(String iResult) {
         System.out.println("==== 結果発表 ====");
-        System.out.println(Player.names[0] + "：" + GameManager.handName(Player.hands[0]));
-        System.out.println(Player.names[1] + "：" + GameManager.handName(Player.hands[1]));
+        System.out.println(Player.sNames[0] + "：" + GameManager.handName(Player.sHands[0]));
+        System.out.println(Player.sNames[1] + "：" + GameManager.handName(Player.sHands[1]));
         System.out.println("------------------");
         System.out.println(iResult);
     }
