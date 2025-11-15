4071e515fd1579830c95030f738e46ddec0279b6
ivy-kat <kataoka@ivy.ac.jp>
2025-11-15 22:03:46 +0900

    整形とコメント追加

--- Judge.java
+++ Judge.java
@@ -1,10 +1,12 @@
 public class Judge {
-    public static String judgeResult(int hand1, int hand2) {
-        if (hand1 == hand2) {
+    public static String judgeResult(int iHand1, int iHand2) {
+        if (iHand1 == iHand2) {     // あいこ
             return GameManager.makeResultMessage("", false, true);
-        } else if ((hand1 == 0 && hand2 == 1) || (hand1 == 1 && hand2 == 2) || (hand1 == 2 && hand2 == 0)) {
+        } else if ((iHand1 == 0 && iHand2 == 1) 
+                    || (iHand1 == 1 && iHand2 == 2) 
+                    || (iHand1 == 2 && iHand2 == 0)) {  // プレイヤー1の勝ち
             return GameManager.makeResultMessage(Player.names[0], true, false);
-        } else {
+        } else {    // プレイヤー2の勝ち
             return GameManager.makeResultMessage(Player.names[1], true, false);
         }
     }
