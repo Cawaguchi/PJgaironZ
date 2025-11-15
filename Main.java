30416f4cc207d242cba7d5374c1ddfe044244a70
ivy-kat <kataoka@ivy.ac.jp>
2025-11-15 22:11:15 +0900

    コメント追加

--- Main.java
+++ Main.java
@@ -1,14 +1,19 @@
 
 public class Main{
     public static void main(String args[]) {
+        // デバッグモードON
         GameManager.sDebugMode=true;
+        // ゲーム開始処理
         GameManager.showTitle();
 
+        // プレイヤーの手選択
         Player.chooseHands();
 
+        // 勝敗判定
         String result=Judge.judgeResult(Player.sHands[0], Player.sHands[1]);
+        // 結果表示
         Display.showResult(result);
-
+        // 終了処理
         GameManager.endGame();
     }
 }
